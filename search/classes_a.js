f56bff4c7df3dbdc33428980300d90e8987ff94b
Stan Jenkins <mc.stanislaw@gmail.com>
2018-03-10 16:05:42 -0500

    auto generated docs from Jenkins

---
+++ search/classes_a.js
@@ -0,0 +1,15 @@
+var searchData=
+[
+  ['map_5frect_5fcombine',['map_rect_combine',['../dd/d7f/classstan_1_1math_1_1internal_1_1map__rect__combine.html',1,'stan::math::internal']]],
+  ['map_5frect_5freduce',['map_rect_reduce',['../d3/dd0/classstan_1_1math_1_1internal_1_1map__rect__reduce.html',1,'stan::math::internal']]],
+  ['map_5frect_5freduce_3c_20f_2c_20double_2c_20double_20_3e',['map_rect_reduce&lt; F, double, double &gt;',['../dd/dc3/classstan_1_1math_1_1internal_1_1map__rect__reduce_3_01_f_00_01double_00_01double_01_4.html',1,'stan::math::internal']]],
+  ['map_5frect_5freduce_3c_20f_2c_20double_2c_20var_20_3e',['map_rect_reduce&lt; F, double, var &gt;',['../dc/de3/structstan_1_1math_1_1internal_1_1map__rect__reduce_3_01_f_00_01double_00_01var_01_4.html',1,'stan::math::internal']]],
+  ['map_5frect_5freduce_3c_20f_2c_20var_2c_20double_20_3e',['map_rect_reduce&lt; F, var, double &gt;',['../da/d01/structstan_1_1math_1_1internal_1_1map__rect__reduce_3_01_f_00_01var_00_01double_01_4.html',1,'stan::math::internal']]],
+  ['map_5frect_5freduce_3c_20f_2c_20var_2c_20var_20_3e',['map_rect_reduce&lt; F, var, var &gt;',['../d7/d61/structstan_1_1math_1_1internal_1_1map__rect__reduce_3_01_f_00_01var_00_01var_01_4.html',1,'stan::math::internal']]],
+  ['multiply_5fmat_5fvari',['multiply_mat_vari',['../da/dd4/classstan_1_1math_1_1multiply__mat__vari.html',1,'stan::math']]],
+  ['multiply_5fmat_5fvari_3c_20double_2c_201_2c_20ca_2c_20tb_2c_201_20_3e',['multiply_mat_vari&lt; double, 1, Ca, Tb, 1 &gt;',['../d8/df6/classstan_1_1math_1_1multiply__mat__vari_3_01double_00_011_00_01_ca_00_01_tb_00_011_01_4.html',1,'stan::math']]],
+  ['multiply_5fmat_5fvari_3c_20double_2c_20ra_2c_20ca_2c_20tb_2c_20cb_20_3e',['multiply_mat_vari&lt; double, Ra, Ca, Tb, Cb &gt;',['../da/d4a/classstan_1_1math_1_1multiply__mat__vari_3_01double_00_01_ra_00_01_ca_00_01_tb_00_01_cb_01_4.html',1,'stan::math']]],
+  ['multiply_5fmat_5fvari_3c_20ta_2c_201_2c_20ca_2c_20double_2c_201_20_3e',['multiply_mat_vari&lt; Ta, 1, Ca, double, 1 &gt;',['../d8/dba/classstan_1_1math_1_1multiply__mat__vari_3_01_ta_00_011_00_01_ca_00_01double_00_011_01_4.html',1,'stan::math']]],
+  ['multiply_5fmat_5fvari_3c_20ta_2c_201_2c_20ca_2c_20tb_2c_201_20_3e',['multiply_mat_vari&lt; Ta, 1, Ca, Tb, 1 &gt;',['../d3/d5d/classstan_1_1math_1_1multiply__mat__vari_3_01_ta_00_011_00_01_ca_00_01_tb_00_011_01_4.html',1,'stan::math']]],
+  ['multiply_5fmat_5fvari_3c_20ta_2c_20ra_2c_20ca_2c_20double_2c_20cb_20_3e',['multiply_mat_vari&lt; Ta, Ra, Ca, double, Cb &gt;',['../d9/da1/classstan_1_1math_1_1multiply__mat__vari_3_01_ta_00_01_ra_00_01_ca_00_01double_00_01_cb_01_4.html',1,'stan::math']]]
+];
---
+++ search/files_9.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['jacobian_2ehpp',['jacobian.hpp',['../d3/d83/fwd_2mat_2functor_2jacobian_8hpp.html',1,'(Global Namespace)'],['../d4/d34/rev_2mat_2functor_2jacobian_8hpp.html',1,'(Global Namespace)']]]
+];
